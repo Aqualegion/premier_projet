e9c4706798df6a63fc14156f92dc4063e108ef26
Yann <yann.martinbaron@gmail.com>
2019-01-22 14:29:11 +0100

    Ajout d'un constructeur avec state valant 0

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,12 @@
 import React, {Component} from 'react';
 
 class Welcome extends Component{
+    constructor(){
+        super();
+        this.state ={
+            count: 0
+        }
+    }
     render() {
         return(
             <h1>Welcome {this.props.name}</h1>
