bcafbec4b9ac03e3e028803494ef560860557ffe
Yann <yann.martinbaron@gmail.com>
2019-01-22 14:11:25 +0100

    Import du composant Welcome et return de celui-ci

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Welcome from './Welcome.js';
 class App extends Component {
   render() {
     return (
-      
+      <Welcome />
     );
   }
 }
