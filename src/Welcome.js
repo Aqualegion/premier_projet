bd920c0c85b8ffe713f6c090d264cfb76e0c3ce4
Yann <yann.martinbaron@gmail.com>
2019-01-22 14:10:52 +0100

    Ajout d'un return renvoyant Welcome

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,1 +1,11 @@
 import react, {Component} from 'react';
+
+class Welcome extends Component{
+    render() {
+        return(
+            <h1>Welcome</h1>
+        );
+    }
+}
+
+export default Welcome;
